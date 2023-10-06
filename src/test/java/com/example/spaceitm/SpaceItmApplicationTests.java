f9da07268f24088f218a726a6dc1a3db2449f012
Asylbek Pishenbay <pishenbai.asilbek@gmail.com>
2023-10-06 16:36:30 +0600

    Initial commit

---
+++ src/test/java/com/example/spaceitm/SpaceItmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spaceitm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceItmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
