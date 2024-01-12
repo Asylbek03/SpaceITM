16b38798c62f409e9f4fb656ac3d60c6e4fc2984
Asylbek03 <117528484+Asylbek03@users.noreply.github.com>
2024-01-12 21:40:56 +0600

    Update Constants.java

--- src/main/java/com/example/spaceitm/util/Constants.java
+++ src/main/java/com/example/spaceitm/util/Constants.java
@@ -2,13 +2,13 @@ package com.example.spaceitm.util;
 
 public class Constants {
 
-    public static final String APOD_URL = "https://api.nasa.gov/planetary/apod?api_key=ZAHIwPnrLy9TdLDLe3cPLVYau4trxcAXGI54dAhJ";
+    public static final String APOD_URL = 
 
-    public static final String EPIC_URL = "https://api.nasa.gov/EPIC/api/natural/images?api_key=ZAHIwPnrLy9TdLDLe3cPLVYau4trxcAXGI54dAhJ";
+    public static final String EPIC_URL = 
 
-    public static final String EPIC_GET_BY_DATE = "https://api.nasa.gov/EPIC/api/natural/all?api_key=ZAHIwPnrLy9TdLDLe3cPLVYau4trxcAXGI54dAhJ";
+    public static final String EPIC_GET_BY_DATE = 
 
-    public static final String MARS_ROVER_URL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=ZAHIwPnrLy9TdLDLe3cPLVYau4trxcAXGI54dAhJ";
+    public static final String MARS_ROVER_URL = 
 
-    public static final String News_Api_URL = "https://api.spaceflightnewsapi.net/v4/articles/";
+    public static final String News_Api_URL = 
 }
